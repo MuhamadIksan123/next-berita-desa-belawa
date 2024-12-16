4983b40a838a23d41ce077f3cf8700d085c50356
Muhamad Iksan <muhamadiksan351@gmail.com>
2024-12-16 10:13:43 +0700

    feat: login

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import localFont from 'next/font/local';
 import './globals.css';
-import ClientWrapper from '@/components/ClientWrapper';
+import ClientWrapper from '@/Components/ClientWrapper';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
